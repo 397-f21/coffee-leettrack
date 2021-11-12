918931403b9baa9d8b06f4dc1e3b17dee446e016
carinaramos <cgramos50@gmail.com>
2021-11-12 16:06:48 -0600

    progress calculator [Carina, Yijing]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
 import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import ProgressIcon from "./Components/ProgressIcon";
 
 function App() {
-  //const [categories, setHasError] = useState();
+  const [categories, setCategories] = useState({
+    "arrays": {
+      "1" : {
+        "name" : "Container with Most Water",
+        "url" : "https://leetcode.com/problems/container-with-most-water",
+        "completed" : 1
+      },
+      "2" : {
+        "name" : "String to Integer",
+        "url" : "https://leetcode.com/problems/string-to-integer-atoi",
+        "completed" : 0
+      },
+      "3" : {
+        "name" : "3Sum",
+        "url" : "https://leetcode.com/problems/3sum",
+        "completed" : 0
+      },
+    },
+    "two pointers": {
+      "1" : {
+        "name" : "Container with Most Water",
+        "url" : "https://leetcode.com/problems/container-with-most-water",
+        "completed" : 1
+      },
+      "2" : {
+        "name" : "String to Integer",
+        "url" : "https://leetcode.com/problems/string-to-integer-atoi",
+        "completed" : 1
+      },
+      "3" : {
+        "name" : "3Sum",
+        "url" : "https://leetcode.com/problems/3sum",
+        "completed" : 0
+      },
+    },
+    "sliding window": {
+      "1" : {
+        "name" : "Container with Most Water",
+        "url" : "https://leetcode.com/problems/container-with-most-water",
+        "completed" : 1
+      },
+      "2" : {
+        "name" : "String to Integer",
+        "url" : "https://leetcode.com/problems/string-to-integer-atoi",
+        "completed" : 1
+      },
+      "3" : {
+        "name" : "3Sum",
+        "url" : "https://leetcode.com/problems/3sum",
+        "completed" : 1
+      }
+    }
+  });
   return (
     <div className="App">
-      <ProgressIcon></ProgressIcon>
+        {Object.keys(categories).map((cateogryName) => (
+            <ProgressIcon 
+              key={cateogryName} 
+              name={cateogryName}
+              questions={categories[cateogryName]}/>
+        ))}
+      
     </div>
   );
 }
--- src/Components/ProgressIcon.js
+++ src/Components/ProgressIcon.js
@@ -1,18 +1,29 @@
 import React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 import "../index.css";
 
-export default function ProgressIcon() {
+export default function ProgressIcon({ name, questions }) {
+
+  const calculateProgress = () => {
+    const isComplete = (question) => question.completed === 1;
+    const total = Object.keys(questions).length;
+    var complete = 0;
+    for (const [id, question] of Object.entries(questions)) {
+      if (isComplete(question)) {
+        complete += 1
+      }
+    }
+    return Math.floor(complete/total*135);
+  }
+
   return (
     <div>
       <div className="card" sx={{ width: 330 }}>
         <div className="cardDescription" variant="h6">
-          Array
+          {name}
         </div>
         <div className="progressBarBack"></div>
-        <div className="progressBarFront"></div>
+        <div className="progressBarFront" style={{width: calculateProgress()}}></div> 
+
       </div>
     </div>
   );
