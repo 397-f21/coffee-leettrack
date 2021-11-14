bd7cadcffafc835d508f89efbe8eafd218c12e72
carinaramos <cgramos50@gmail.com>
2021-11-14 15:29:57 -0600

    random question selection [Carina, Iris]

--- src/App.js
+++ src/App.js
@@ -33,17 +33,20 @@ function App() {
         "1" : {
           "name" : "Container with Most Water",
           "url" : "https://leetcode.com/problems/container-with-most-water",
-          "completed" : 1
+          "completed" : 1,
+          "reviewAgain" : 0
         },
         "2" : {
           "name" : "String to Integer",
           "url" : "https://leetcode.com/problems/string-to-integer-atoi",
-          "completed" : 1
+          "completed" : 1,
+          "reviewAgain" : 0
         },
         "3" : {
           "name" : "3Sum",
           "url" : "https://leetcode.com/problems/3sum",
-          "completed" : 0
+          "completed" : 0,
+          "reviewAgain" : 0
         }
       }
     },
@@ -53,17 +56,20 @@ function App() {
         "1" : {
           "name" : "Container with Most Water",
           "url" : "https://leetcode.com/problems/container-with-most-water",
-          "completed" : 1
+          "completed" : 1,
+          "reviewAgain" : 0
         },
         "2" : {
           "name" : "String to Integer",
           "url" : "https://leetcode.com/problems/string-to-integer-atoi",
-          "completed" : 1
+          "completed" : 1,
+          "reviewAgain" : 1
         },
         "3" : {
           "name" : "3Sum",
           "url" : "https://leetcode.com/problems/3sum",
-          "completed" : 1
+          "completed" : 1,
+          "reviewAgain" : 0
         }
       }
     },
@@ -73,27 +79,69 @@ function App() {
         "1" : {
           "name" : "Container with Most Water",
           "url" : "https://leetcode.com/problems/container-with-most-water",
-          "completed" : 1
+          "completed" : 1,
+          "reviewAgain" : 0
         },
         "2" : {
           "name" : "String to Integer",
           "url" : "https://leetcode.com/problems/string-to-integer-atoi",
-          "completed" : 0
+          "completed" : 0,
+          "reviewAgain" : 0
         },
         "3" : {
           "name" : "3Sum",
           "url" : "https://leetcode.com/problems/3sum",
-          "completed" : 1
+          "completed" : 1,
+          "reviewAgain" : 1
         }
       }
     }
   });
+  const [dailyGoal, setDailyGoal] = useState(2);
+  const [newProblems, setNewProblems] = useState([]);
+  const [reviewProblems, setReviewProblems] = useState([]);
+
+  // function to select problems the user would like to review
+  const selectReviewProblems = () => {
+    // iterate through all problems to find all
+    // which have the reviewAgain attribute of 1
+    var problemsToReview = [];
+    for (const [id, category] of Object.entries(categories)) {
+      for (const [id, question] of Object.entries(category.questions)) {
+        if (question.reviewAgain === 1) {
+          problemsToReview.push(question)
+        }
+      }
+    }
+    // select dailyGoal random problems from that list
+    const shuffled = problemsToReview.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, dailyGoal);
+  }
+  // function to select random problems that have not been completed
+  const selectRecommendedProblems = () => {
+    // iterate through all problems to find all
+    // which have the completed attribute of 0
+    var problemsToDo = [];
+    for (const [id, category] of Object.entries(categories)) {
+      for (const [id, question] of Object.entries(category.questions)) {
+        if (question.completed === 0) {
+          problemsToDo.push(question)
+        }
+      }
+    }
+    // select dailyGoal random problems from that list
+    const shuffled = problemsToDo.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, dailyGoal);
+  }
+
+
+
   return (
     <div className="App">
       <Typography variant="h3" > LeetTrack</Typography>
       <div className="content">
         <div id="progress-icon-list">
-        <Typography variant="h5" > Overall Progress:65%</Typography>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}> Overall Progress: 65%</Typography>
           {Object.keys(categories).map((categoryName) => (
             <ProgressIcon 
               key={categoryName} 
@@ -103,18 +151,31 @@ function App() {
         </div>
         <div id="problems-list">
           <div className="problem-list-header">
-            <Typography variant="h5" > Daily Goal: 2 problems</Typography>
+            <Typography variant="h5" sx={{ fontWeight: 'bold' }}> Daily Goal: {dailyGoal} problems</Typography>
             <button>edit</button>
           </div >
-            <div id="problems">
-              <div>
-                <p>gfyrfgyrgfyrgyr</p>
-
-              </div>
-              <div>
-              <p>gfyrfgyrgfyrgyr</p>
-              </div>
+          <br/>
+          <div>
+            <div id="reviewProblems">
+            <Typography variant="h5" sx={{ fontWeight: 'medium' }}> Problems to Review:</Typography>
+              <ol>
+                {selectReviewProblems().map((question) =>
+                  <li>
+                    <a href={question.url}>{question.name}</a>
+                  </li>                  )}
+              </ol>
             </div>
+            <div id="newProblems">
+              <Typography variant="h5" sx={{ fontWeight: 'medium' }}> Recommended New Problems:</Typography>
+              <ol>
+                {selectRecommendedProblems().map((question) =>
+                  <li>
+                    <a href={question.url}>{question.name}</a>
+                  </li>
+                )}
+              </ol>
+            </div>
+          </div>
         </div>
   
       </div>
